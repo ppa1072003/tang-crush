e9ede6406750a0446ef70212672992b00ec1e24f
ppa1072003 <110301888+ppa1072003@users.noreply.github.com>
2022-07-31 00:23:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Special day of Đỗ Anh Quân 😙',
+    desc: 'Cậu đồng ý làm người yêu tớ nhé?, Đỗ Anh Quân',
+    btnYes: 'Tớ đồng ý liền <33',
+    btnNo: 'Để tớ suy nghĩ :3',
+    question:'Hihi cậu đồng ý hỏ <3',
+    btnReply: 'Tớ biết là Đỗ Anh Quân cũng iuu tớ mòo <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Cùng nhau cố gắng .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
